710bca028f64ccb3009dbe0afdcf63c124b60bf7
Ctix <ctix@ibmx61>
2018-08-27 21:00:32 +0800

    fist commit the raw materails and raw codes

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # stockgraphics
 sorts of stock daily charts , real time curve graph etc , make and show the usage of pyqtgraph 
+
+
+## TASKS
+
+### Display the bidding volume distribution along the Horizontal X axis 
+1. Draw Horizontal Line from the Vertical Y axis , representing the volume of 
+    each price bidding.
+
+
+## Daily Time Spending Recorder
+ 2 hours to disable pep8 post write syntax checking   done!!
+    1. Jobs down , created a Class of representing the Graphic Items 
+    2. Vertically Drawing Volumes of the spreading bidding prices . spectrum 
+    3. the solo goal to fulfill the dramatic drawing line graph of the stock real time 
+    plotting , receiving data from hq.sina.com
+    1. pack the lines of data to a data frame of pandas
+    1. stack all of the each of bidding price, corresponding volume to the Vertical 
+    1. Test graph items setting layout.
+    1. the order of the developing is in  such a sequence as below described:
+        *  deteriment the layout , used the examples code
+        *  capsulating the datum , price line data then volume data 
+        * Display in one same plotting eara .
+    1.  sh index to candle sticks
+
+### modify the example codes of pyqtgraphic , candlesticks.py 
+### Learn to implementing the various features of pyqtgraphic tools  
---
+++ layout.py
@@ -0,0 +1,100 @@
+"""
+Demonstrates some layout and customized mouse
+interaction by drawing a crosshair that follows
+the mouse.
+
+"""
+
+import numpy as np
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+from pyqtgraph.Point import Point
+
+#generate layout
+app = QtGui.QApplication([])
+win = pg.GraphicsWindow()
+win.setWindowTitle('pyqtgraph example: crosshair')
+label = pg.LabelItem(justify='right')
+win.addItem(label)
+for i in range(1,4):
+    c1 = win.addLabel("col ",0,i)
+    # c1 = win.addLabel("column 2 ",0,1)
+    # c1 = win.addLabel("column 3 ",0,2)
+    # c1 = win.addLabel("column4 ",0,3)
+    r1 = win.addLabel("r1 ",i,0)
+    # r1 = win.addLabel("row2 ",2,0)
+    # r1 = win.addLabel("row3 ",3,0)
+
+p0 = win.addPlot(row=1, col=1,rowspan =2 ,colspan=1)
+p1 = win.addPlot(row=1, col=2,rowspan =2 ,colspan=2)
+p2 = win.addPlot(row=3, col=1, colspan=3)
+
+region = pg.LinearRegionItem()
+region.setZValue(10)   # wut the hell of this means ??
+# Add the LinearRegionItem to the ViewBox, but tell the ViewBox to exclude this
+# item when doing auto-range calculations.
+p2.addItem(region, ignoreBounds=True)
+
+#pg.dbg()
+p1.setAutoVisible(y=True)
+
+
+#create numpy arrays
+#make the numbers large to show that the xrange shows data from 10000 to all the way 0
+data0 = 10+ 15000 * pg.gaussianFilter(np.random.random(size=100), 10)\
+            + 3000 * np.random.random(size=100)
+data1 = 10000 + 15000 * pg.gaussianFilter(np.random.random(size=10000), 10) + 3000 * np.random.random(size=10000)
+data2 = 15000 + 15000 * pg.gaussianFilter(np.random.random(size=10000), 10) + 3000 * np.random.random(size=10000)
+
+p0.plot(data0, pen="y")
+p1.plot(data1, pen="r")
+p1.plot(data2, pen="g")
+
+p2.plot(data1, pen="w")
+
+def update():
+    region.setZValue(10)
+    minX, maxX = region.getRegion()
+    p1.setXRange(minX, maxX, padding=0)
+
+region.sigRegionChanged.connect(update)
+
+def updateRegion(window, viewRange):
+    rgn = viewRange[0]
+    region.setRegion(rgn)
+
+p1.sigRangeChanged.connect(updateRegion)
+
+region.setRegion([1000, 2000])
+
+#cross hair
+vLine = pg.InfiniteLine(angle=90, movable=False)
+hLine = pg.InfiniteLine(angle=0, movable=False)
+p1.addItem(vLine, ignoreBounds=True)
+p1.addItem(hLine, ignoreBounds=True)
+
+
+vb = p1.vb
+
+def mouseMoved(evt):
+    pos = evt[0]  ## using signal proxy turns original arguments into a tuple
+    if p1.sceneBoundingRect().contains(pos):
+        mousePoint = vb.mapSceneToView(pos)
+        index = int(mousePoint.x())
+        if index > 0 and index < len(data1):
+            label.setText("<span style='font-size: 12pt'>x=%0.1f,   <span style='color: red'>y1=%0.1f</span>,   <span style='color: green'>y2=%0.1f</span>" % (mousePoint.x(), data1[index], data2[index]))
+        vLine.setPos(mousePoint.x())
+        hLine.setPos(mousePoint.y())
+
+
+
+proxy = pg.SignalProxy(p1.scene().sigMouseMoved, rateLimit=60, slot=mouseMoved)
+#p1.scene().sigMouseMoved.connect(mouseMoved)
+
+
+## Start Qt event loop unless running in interactive mode or using pyside.
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
+
