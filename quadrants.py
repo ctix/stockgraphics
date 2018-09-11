34b1944fb65656b4b10812aa46ca6e777ba272e1
Ctix <ctix@ibmx61>
2018-09-11 16:02:13 +0800

    vol line and title show price

--- quadrants.py
+++ quadrants.py
@@ -18,6 +18,12 @@ from pyqtgraph.Point import Point
 import pickle
 from realtimeline import RetrieveOnLine, DueTime, AtTransactionTime
 
+def minMaxRange(x,range_values):
+    """scale data list to fit the range values"""
+    return [round( ((xx-min(x))/(max(x)-min(x)))*\
+                    (range_values[1] - range_values[0])+\
+            range_values[0],2) for xx in x]
+
 # Below code should be observed if duetime past ,should executed immediately
 now = datetime.datetime.now()
 print(str(now))
@@ -25,7 +31,7 @@ stlist = ["sz300474","sz002049","sz000977","sz002642"]
 test_re = RetrieveOnLine(stlist, 30)
 
 ### pickle save /load the previous data
-pkl_file = open('stdata.pkl', 'rb')
+pkl_file = open('stdata1.pkl', 'rb')
 pre_data = pickle.load(pkl_file)
 pkl_file.close()
 
@@ -42,29 +48,43 @@ win.setWindowTitle('pyqtgraph example: Plotting')
 pg.setConfigOptions(antialias=True)
 
 p0 = win.addPlot(title="jjw300474" )
-curve0 = p0.plot(pre_data[0]["price"] ,pen= "r")
-p0.showGrid(x=True, y=True, alpha=0.5)
+p0.setTitle
+# curve0 = p0.plot(pre_data[0]["price"] ,pen= "r")
+p0.showGrid(x=True, y=True, alpha=0.7)
 p1 = win.addPlot(title="zggw002049")
-curve1 = p1.plot(pre_data[1]["price"],pen = "g")
-p1.showGrid(x=True, y=True, alpha=0.6)
+# curve1 = p1.plot(pre_data[1]["price"],pen = "g")
+p1.showGrid(x=True, y=True, alpha=0.7)
 
-# p1.plot(np.random.normal(size=100), pen=(255,0,0), name="Red curve")
-# p1.plot(np.random.normal(size=110)+5, pen=(0,255,0), name="Green curve")
-# p1.plot(np.random.normal(size=120)+10, pen=(0,0,255), name="Blue curve")
 win.nextRow()
 p2 = win.addPlot(title="lcxx000977")
-p2.showGrid(x=True, y=True, alpha=0.7)
-curve2 = p2.plot(pen = "g")
-
+p2.showGrid(x=True, y=True, alpha=0.8)
+curve2 = p2.plot(pen = "y")
 
 p3 = win.addPlot(title="rzl002642")
 p3.showGrid(x=True, y=True, alpha=0.9)
-curve3 = p3.plot(pen = "w")
+curve3 = p3.plot(pen = "b")
 
 #define the dynamic variable names
 print(list(enumerate(stlist)))
-# for i,stc in enumerate(stlist):
-    # exec("curve{}=p{}.plot(pen='y')".format(i))
+# define the downwards size
+down = 0.4
+
+#merge the pickle load data to the datalines
+test_re.datalines = pre_data
+for i,pdata in enumerate(test_re.datalines):
+    # print(pdata['vol'][:30])
+    if len(pdata['vol'])>2:
+        voldata = list(np.array(pdata['vol'][1:])-np.array(pdata['vol'][:-1]))
+        print(voldata[:20])
+        mi, mx = min(pdata['price']),max(pdata['price'])
+        # voldata = minMaxRange(pdata['vol'], (mi-down,mx-down))
+        voldata = minMaxRange(voldata, (mi-down,mx-down))
+        print(voldata[:20])
+    # exec("vol_curve{0}=p{0}.plot(voldata,pen='y',brush=(200,200,233),\
+         # fillLevel=0)".format(i))
+        exec("vol_curve{0}=p{0}.plot(voldata,pen='y')".format(i))
+    exec("curve{0}=p{0}.plot(pdata['price'],pen='r',)".format(i))
+
 
 def update():
     if AtTransactionTime() ==  "after":
@@ -72,7 +92,7 @@ def update():
         pickle.dump(test_re.datalines,stdatafile)
         time.sleep(300)
         print("Market Closed Alreedy!!!")
-        stdatafile.close()
+        # stdatafile.close()
         return 0
     if AtTransactionTime() == "noon" :
         # sleep until 13:00,think recover Retrieving exactly on time
@@ -85,12 +105,16 @@ def update():
         test_re.getStockData(line_data["name"])
         print(line_data["name"],line_data["price"])
         exec("curve{}.setData(line_data['price'])".format(i))
-        # exec("vol_curve{}.setData(line_data['vol'])".format(i))
+        exec("vol_curve{}.setData(line_data['vol'])".format(i))
+        now_price = line_data["price"][-1]
+        exec("p{0}.setTitle('{1}@price ={2}')".\
+             .format(i,line_data["name"],now_price))
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
 # interval 29 sec to update the plotting
 timer.start(23000)
+# timer.start(999000)  ## not updating the plotting ,for the trial
 
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
