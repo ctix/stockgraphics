a6c2ecc8285f63cca1364559396a7a043e0a81e1
Ctix <ctix@ibmx61>
2019-06-13 09:29:02 +0800

    config UI added

--- qtconfigui.py
+++ qtconfigui.py
@@ -13,26 +13,37 @@ class MainWindow(QMainWindow):
         self.ui = loadUi('./config/configFace.ui', self)
         #  self.setFixedSize(self.sizeHint())
 
-        configfn = "./config/stocks.ini"
-
+        _configfn = "./config/stocks.ini"
+        self.file_lines = self.read_config_to_list(_configfn)
         self.qall_list = []
         self.moni_list = []
-        self.cfg = ConfigOfStocks(configfn)
+        self.cfg = ConfigOfStocks(_configfn)
 
         self.set_listview(self.ui.monitoredListView, "monitored")
         self.set_listview(self.ui.favorListView, "all")
 
         self.ui.favorListView.clicked.connect(self.lv_clicked)
 
+    def read_config_to_list(self, file_name):
+        _file_lines = []
+        with open(file_name, "r") as fh:
+            #  return fh.readlines()
+            for line in fh.readlines():
+                line = line[:-1]    # cut the tail "\n"
+                _file_lines.append(line)
+
+        return _file_lines
+
     def set_listview(self, LV, sect_name):
         slm = QStringListModel()
         if sect_name == "all":
-            stlist = self.cfg.get_all_items_lst()
-            self.qall_list = list(stlist)
+            stlist = self.file_lines
+            self.qall_list = list(self.file_lines)
         else:
             stlist = self.cfg.get_section_value_list(sect_name)
             self.moni_list = list(stlist)
 
+        print("show the list ==>{}".format(stlist))
         slm.setStringList(stlist)
         #  self.ui.monitoredListView.setModel(slm)
         LV.setModel(slm)
@@ -47,3 +58,5 @@ app = QApplication(sys.argv)
 w = MainWindow()
 w.show()
 sys.exit(app.exec())
+
+
