79f9b8822f5236c3cb349904d38310415015376e
Ctix <ctix@ibmx61>
2019-06-25 08:54:09 +0800

    add crontab config,move to cvm

---
+++ config/acquiringHQ.sh
@@ -0,0 +1,6 @@
+pkill -f "python acquiring_hq.py"
+sleep 5
+cd  /home/ctix/Dev/github/stockgraphics/pw/
+/home/ctix/anaconda3/bin/python acquiring_hq.py &
+
+
---
+++ config/sanicRest.sh
@@ -0,0 +1,6 @@
+
+pkill -f "python sanic_main.py"
+sleep 5
+cd /home/ctix/Dev/github/stockgraphics/pw/
+/home/ctix/anaconda3/bin/python sanic_main.py &
+
